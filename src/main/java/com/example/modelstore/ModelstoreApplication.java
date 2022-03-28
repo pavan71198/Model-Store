3112b60cd670943d4171993ebd4f9f134fd644f7
Pavan Kumar <pavan71198@users.noreply.github.com>
2022-03-29 00:42:13 +0530

    SpringBoot project initialisation

---
+++ src/main/java/com/example/modelstore/ModelstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.modelstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelstoreApplication.class, args);
+	}
+
+}
